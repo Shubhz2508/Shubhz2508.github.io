8bbacee588ae1f01014004034413c4356e92ea0f
Shubhz2508 <117626394+Shubhz2508@users.noreply.github.com>
2023-09-05 23:23:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -43,3 +43,44 @@ for (var i = 0; i < navLinks.length; i++) {
     menuBtn.style.pointerEvents = "auto";
   });
 }
+let nb_stops = 10; // 10 color stops should be enough
+let dir = "left"; // left, right, top, bottom
+
+function SetupRainbow() {
+  var rainbowStr = GetRainbowString(nb_stops, 80, 50);
+  var oppositeDir = (dir==="left"?"right":(dir==="right"?"left":(dir==="top"?"bottom":"top")));
+  var css = '.rainbowText {\
+   background-clip: text;\
+   color: transparent;\
+   -webkit-background-clip: text;\
+   -webkit-text-fill-color: transparent;\
+   background-image: -webkit-linear-gradient(' + dir + ',' + rainbowStr + '); \
+   background-image: linear-gradient(to ' + oppositeDir + ',' + rainbowStr + ') \
+}'
+
+  var style = document.createElement("style");
+  style.type = 'text/css';
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  document.head.appendChild(style);
+}
+
+
+// function that generate the rainbow string
+function GetRainbowString(nbStops, saturation, luminosity) {
+  let gap = 360 / nbStops,
+    colors = [];
+  for (let i = 0; i < nbStops; i++) {
+    colors.push("hsl(" + (i * gap) + "," + saturation + "%," + luminosity + "%)");
+  }
+  return colors.join();
+}
+
+
+
+window.addEventListener("load", function() {
+  SetupRainbow();
+});
